a5e82c1d6261fac7abb5db4998723d7e982d3b92
GustavoBiava <gustavo.rbiava@hotmail.com>
2024-07-21 18:14:29 -0300

    Sequelize config & Aluno model

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.status(200).json({
-      tudoCerto: true
+  async index(req, res) {
+    const aluno = await Aluno.create({
+      nome: 'Carlos',
+      sobrenome: 'Sainz',
+      email: 'Chilli@email.com',
+      idade: 29,
+      peso: 66,
+      altura: 1.78
     });
+    res.status(200).json({ aluno });
   }
 }
 
