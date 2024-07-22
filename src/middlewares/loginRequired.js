56756362fb73d3a923a4167aa8f2a8d131b12935
GustavoBiava <gustavo.rbiava@hotmail.com>
2024-07-21 22:55:27 -0300

    LoginRequired middleware (JWT Token)

---
+++ src/middlewares/loginRequired.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ errors: ['Login necessário'] });
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+    const { id, email } = payload;
+    req.userId = id;
+    req.userEmail = email;
+    return next();
+  }
+  catch (err) {
+    return res.status(401).json({ errors: ['Token inválido ou expirado'] });
+  }
+};
