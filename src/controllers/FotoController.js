b00ead2a978972ea81908ece00c872e83399ada7
GustavoBiava <gustavo.rbiava@hotmail.com>
2024-07-23 00:08:47 -0300

    LoginRequired to upload & saving photos on DB

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,14 +1,23 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import Foto from '../models/Foto';
 
 const upload = multer(multerConfig).single('archive');
 
 class FotoController {
-  async store(req, res) {
-    return upload(req, res, (err) => {
+  store(req, res) {
+    return upload(req, res, async (err) => {
       if (err) return res.status(400).json({ errors: [err.code] });
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await Foto.create({ original_name: originalname, filename, aluno_id });
+        return res.status(200).json(foto);
+      }
+      catch (e) {
+        return res.status(400).json({ errors: e.errors.map((error) => error.message) });
+      }
 
-      return res.status(200).json(req.file);
     });
   }
 }
